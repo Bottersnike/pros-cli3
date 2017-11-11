1ad98d05f10bc7a1eb817631cd9a40aeb833957a
Alex Brooke <akb.sbc@gmail.com>
2017-11-11 02:38:44 -0500

    add stubs for retrieving online and offline templates through provider

--- pros/conductor/depots/depot.py
+++ pros/conductor/depots/depot.py
@@ -1,6 +1,6 @@
 import os
 
-from pros.conductor.templates import TemplateIdentifier
+from pros.conductor.templates.template_identifier import TemplateIdentifier
 from .depot_config import DepotConfig
 
 class Depot(object):
@@ -24,22 +24,27 @@ class Depot(object):
         else:
             raise ValueError("Couldn't deduce meaning of {}".format(arg))
 
+    def get_online_templates(self):
+        pass
+
+    def get_offline_templates(self):
+        pass
+
+    def get_all_templates(self):
+        return [self.get_online_templates(), self.get_offline_templates()]
+
     @classmethod
     def install_template(cls, template):
-        # TODO: make Template from TemplateIdentifier and call its install function
-        pass
+        template.install()
 
     @classmethod
     def uninstall_template(cls, template):
-        # TODO: make Template from TemplateIdentifier and call its uninstall function
-        pass
+        template.uninstall()
 
     @classmethod
     def upgrade_template(cls, template):
-        # TODO: make Template from TemplateIdentifier and call its upgrade function
-        pass
+        template.upgrade()
 
     @classmethod
     def delete_template(cls, template):
-        # TODO: make Template from TemplateIdentifier and call its delete function
-        pass
+        template.delete()
